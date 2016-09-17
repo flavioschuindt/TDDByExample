4f889d4b1d5318bdd752cdddf78479f70127350d
Flávio Schuindt <flavio.schuindt@ge.com>
2016-09-16 23:51:54 -0300

    Chapter 3 done.

---
+++ tddbyexample/src/main/java/com/flavioschuindt/tddbyexample/Dollar.java
@@ -0,0 +1,20 @@
+package com.flavioschuindt.tddbyexample;
+
+public class Dollar {
+	
+	int amount;
+
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
+	
+	Dollar times(int multiplier){
+		return new Dollar(amount * multiplier);
+	}
+	
+	public boolean equals(Object object){
+		Dollar dollar = (Dollar)object;
+		return amount == dollar.amount;
+	}
+
+}
---
+++ tddbyexample/src/test/java/com/flavioschuindt/tddbyexample/AppTest.java
@@ -0,0 +1,52 @@
+package com.flavioschuindt.tddbyexample;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+    public void testMultiplication() {
+    	Dollar five = new Dollar(5);
+    	Dollar product = five.times(2);
+    	assertEquals(10, product.amount);
+    	product = five.times(3);
+    	assertEquals(15, product.amount);
+    }
+    
+    public void testEquality() {
+    	assertTrue(new Dollar(5).equals(new Dollar(5)));
+    	assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+    
+}
