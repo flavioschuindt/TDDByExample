646b00983535ce8a6557ae976970d798d9b50214
Flávio Schuindt <flavio.schuindt@ge.com>
2016-09-17 17:30:21 -0300

    Chapter 9 done.

--- tddbyexample/src/test/java/com/flavioschuindt/tddbyexample/AppTest.java
+++ tddbyexample/src/test/java/com/flavioschuindt/tddbyexample/AppTest.java
@@ -56,4 +56,9 @@ public class AppTest
     	assertEquals(Money.franc(15), five.times(3));
     }
     
+    public void testCurrency() {
+    	assertEquals("USD", Money.dollar(1).currency());
+    	assertEquals("CHF", Money.franc(1).currency());
+    }
+    
 }
