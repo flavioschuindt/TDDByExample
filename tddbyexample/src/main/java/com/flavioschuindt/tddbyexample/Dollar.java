dabcc6c3cfc021b4b88b1140109246a323399dc2
Flávio Schuindt <flavio.schuindt@ge.com>
2016-09-17 15:43:38 -0300

    Chapter 6 done.

--- tddbyexample/src/main/java/com/flavioschuindt/tddbyexample/Dollar.java
+++ tddbyexample/src/main/java/com/flavioschuindt/tddbyexample/Dollar.java
@@ -1,8 +1,6 @@
 package com.flavioschuindt.tddbyexample;
 
-public class Dollar {
-	
-	private int amount;
+public class Dollar extends Money {
 
 	public Dollar(int amount) {
 		this.amount = amount;
@@ -10,11 +8,6 @@ public class Dollar {
 	
 	Dollar times(int multiplier){
 		return new Dollar(amount * multiplier);
-	}
-	
-	public boolean equals(Object object){
-		Dollar dollar = (Dollar)object;
-		return amount == dollar.amount;
 	}
 
 }
--- tddbyexample/src/main/java/com/flavioschuindt/tddbyexample/Franc.java
+++ tddbyexample/src/main/java/com/flavioschuindt/tddbyexample/Franc.java
@@ -1,8 +1,6 @@
 package com.flavioschuindt.tddbyexample;
 
-public class Franc {
-	
-	private int amount;
+public class Franc extends Money {
 
 	public Franc(int amount) {
 		this.amount = amount;
@@ -10,11 +8,6 @@ public class Franc {
 	
 	Franc times(int multiplier){
 		return new Franc(amount * multiplier);
-	}
-	
-	public boolean equals(Object object){
-		Franc dollar = (Franc)object;
-		return amount == dollar.amount;
 	}
 
 }
